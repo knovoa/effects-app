f124037357886cc45921e7c311a9cb877dcd3e43
Kevin Novoa <kevnovoa94.kn@gmail.com>
2021-09-01 00:14:37 -0500

    D3 poc added. Pending to set markers

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -5,6 +5,11 @@ import { User } from 'src/app/models/user.model';
 import { load } from 'src/app/store/actions/users.actions';
 import { AppState } from 'src/app/store/app.reducers';
 
+import * as d3 from 'd3';
+import * as topojson from 'topojson-client';
+
+let cityName: '';
+
 @Component({
   selector: 'app-users-list',
   templateUrl: './list.component.html',
@@ -16,17 +21,15 @@ export class ListComponent implements OnInit {
   loading: boolean = false;
   error: any;
 
-  myType = ChartType.GeoChart;
+  //cityName: String = '';
+
+  myType = ChartType.Table;
   myOptions = {
     //displayMode: 'markers',
     region: 'PE',
     resolution: 'provinces',
     defaultColor: '#C3CBCD',
-    datalessRegionColor: '#123456',
-    backgroundColor: {
-      stroke: 'black',
-      strokeWidth: 1
-    }
+    datalessRegionColor: '#123456'
   }
   myData = [
     ['Regions'],
@@ -67,11 +70,79 @@ export class ListComponent implements OnInit {
       this.loading = users.loading;
       this.error = users.error;
     });
+
+    this.draw();
   }
 
   onSelect($event: ChartSelectionChangedEvent) {
     if ($event.selection.length > 0) {
       console.log($event.selection[0].row);
     }
+  }
+
+  async draw() {
+    const topology: any = await d3.json('/assets/peru.json');
+
+    const geojson = topojson.feature(topology, topology.objects.departments)
+    const svg = d3.select('svg').attr('id', 'map')
+    const projection = d3.geoAlbers()
+    const path = d3.geoPath().projection(projection)
+
+    let width = 750;
+    let height = 750;
+    projection.rotate([60, 40]).fitExtent([ [ 0, 0 ], [ width, height ] ], geojson);
+
+    let cities = [
+      {
+        lng: -12.30200325453761,
+        lat: -76.57102540839436
+      },
+      {
+        lng: -4.269263755674196,
+        lat: -74.47440370503764
+      },
+      {
+        lng: -17.512823050613296,
+        lat: -70.19380106068432
+      }
+    ];
+
+    svg
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('path')
+      .data(geojson.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .attr('fill', '#DCE1EA')
+      .attr('stroke', 'white')
+      .on('mouseover', function(d) {
+        d3.select(this).classed('selected', true);
+      })
+      .on('mouseout', function(d) {
+        d3.select(this).classed('selected', false);
+      })
+      .on('click', function(d,i: any) {
+        cityName = i.properties.NOMBDEP;
+      });
+
+    svg
+      .selectAll("g")
+      .data(cities)
+      .enter()
+      .append("g")
+      .attr("transform", function(d) {
+        return "translate(" + projection([d.lng, d.lat]) + ")";
+      })
+      .append("circle")
+      .attr("r", 1)
+  }
+
+  demo() {
+    if (cityName === undefined) {
+      return;
+    }
+    alert('City: ' + cityName);
   }
 }
